a8c4d692d8c0bf1517b726d081fba29f16dada92
BBakula <bozo.bakula@gmail.com>
2023-01-12 12:34:55 +0100

    update

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ({switchTheme}) => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={isDarkMode ? images.bblogo2 : images.bblogo1} alt="logo" />
+        <img src={isDarkMode ? images.bblogo1 : images.bblogo2} alt="logo" />
       </div>
       <ul className="app__navbar-links">
         {['home', 'skills', 'work', 'about', 'contact'].map((item) => (
